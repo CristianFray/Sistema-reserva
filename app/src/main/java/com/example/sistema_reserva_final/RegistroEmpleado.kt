ecb37639e5988840e272e459066ab4bbdb60f560
fray <chicofray02@gmail.com>
2021-11-08 21:01:58 -0500

    iniciar el proyecto

---
+++ app/src/main/java/com/example/sistema_reserva_final/RegistroEmpleado.kt
@@ -0,0 +1,11 @@
+package com.example.sistema_reserva_final
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistroEmpleado : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro_empleado)
+    }
+}
